2da5bd1dbfd57e3261bc1c6f24c7f1270b687f0f
Dawid Studzizba <studzizba.dawid@gmail.com>
2023-10-24 18:07:20 +0200

    Keep track of the high score state

--- scoreboard.py
+++ scoreboard.py
@@ -9,7 +9,8 @@ class Scoreboard(Turtle):
         super().__init__()
         self.penup()
         self.score = 0
-        self.high_score = 0
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
         self.color("white")
         self.goto(x=0, y=265)
         self.hideturtle()
@@ -26,5 +27,7 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open("data.txt", mode="w") as data:
+                data.write(str(self.high_score))
         self.score = 0
         self.update_scoreboard()
